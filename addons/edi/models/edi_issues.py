092d1e8d5275a2b04dd477fe5facacb83db21e39
Miquel PS <miquel.palahi.sitges@unipart.io>
2024-10-25 10:25:09 +0100

    [IMP] edi: Create BaseIssue class

--- addons/edi/models/edi_issues.py
+++ addons/edi/models/edi_issues.py
@@ -35,6 +35,127 @@ class ProjectTask(models.Model):
     )
 
 
+class BaseIssue(models.AbstractModel):
+    """Base Issue-Tracked Object"""
+
+    # To consider: Would be good to move this abstract class in a module that only depends
+    # on project module in the future. So if we want to replicate this solution not to depend on
+    # EDI module by force.
+    _name = "base.issues"
+    _inherit = "mail.thread"
+    _description = "Base Issue Object"
+
+    # Empty list variable, will be overriden in the specific models where used.
+    ISSUES_FIELD_MAP = []
+
+    issue_ids = fields.One2many(
+        "project.task",
+        string="Issues",
+        domain=["|", ("stage_id.fold", "=", False), ("stage_id", "=", False)],
+    )
+    issue_count = fields.Integer(string="Issue Count", compute="_compute_issue_counts", store=True)
+    rel_issue_count = fields.Integer(
+        string="Related Issue Count", compute="_compute_issue_counts", store=True
+    )
+
+    @api.model
+    def display_issue_counts_depends(self):
+        """
+        Base fields which _compute_issue_counts depends on, it will be extended with super call
+        on specific models where used.
+        """
+        return ["issue_ids", "issue_ids.stage_id"]
+
+    @api.depends(lambda self: self.display_issue_counts_depends())
+    def _compute_issue_counts(self):
+        """Compute number of open issues (for UI display)"""
+        inverse = self._fields["issue_ids"].inverse_name
+        related = [
+            base_field
+            for field, base_field in self.ISSUES_FIELD_MAP
+            if base_field != inverse and field not in self._fields
+        ]
+        for issues in self:
+            issues.issue_count = len(
+                issues.issue_ids.filtered(lambda x: all(not getattr(x, f) for f in related))
+            )
+            issues.rel_issue_count = len(issues.issue_ids) - issues.issue_count
+
+    def _issue_vals(self):
+        """Construct values for corresponding issues"""
+        self.ensure_one()
+        vals = {'project_id': self.project_id.id}
+        vals[self._fields['issue_ids'].inverse_name] = self.id
+        for field, base_field in self.ISSUES_FIELD_MAP:
+            if hasattr(self, field):
+                rec = getattr(self, field)
+                if rec:
+                    vals[base_field] = rec.id
+        return vals
+
+    def raise_issue(self, fmt, err):
+        """Raise issue via issue tracker
+
+        Raise an issue in the associated issue tracker.
+        """
+        self.ensure_one()
+
+        # Parse exception
+        title = err.name if isinstance(err, UserError) else str(err)
+        tbe = traceback.TracebackException.from_exception(err)
+
+        # Construct issue
+        vals = self._issue_vals()
+        vals['name'] = ("[%s] %s" % (self.name, title))
+        issue = self.env['project.task'].create(vals)
+
+        # Construct list of threads
+        threads = [self]
+        for field, _issue_field in self.ISSUES_FIELD_MAP:
+            if field in self._fields:
+                thread = getattr(self, field)
+                if thread:
+                    threads += thread
+
+        # Add traceback if applicable
+        trace = ''.join(tbe.format())
+        _logger.error(trace)
+        if not isinstance(err, UserError):
+            issue.message_post(body=trace, content_subtype='plaintext')
+            for thread in threads:
+                thread.sudo().message_post(body=trace,
+                                           content_subtype='plaintext')
+
+        # Add summary
+        for thread in threads:
+            thread.sudo().message_post(body=(fmt % title),
+                                       content_subtype='plaintext')
+        return issue
+
+    def close_issues(self):
+        """Close all open issues"""
+        for issue in self.mapped('issue_ids'):
+            closed = issue.stage_find(issue.project_id.id,
+                                      [('fold', '=', True)])
+            issue.stage_id = closed
+
+    def action_view_issues(self):
+        """View open issues"""
+        self.ensure_one()
+        action = self.env.ref('project.action_view_task').read()[0]
+        action['domain'] = [(self._fields['issue_ids'].inverse_name,
+                             '=', self.id)]
+        action['context'] = {'default_%s' % k: v
+                             for k, v in self._issue_vals().items()}
+        action['context'].update({'create': True})
+        return action
+
+    def action_close_issues(self):
+        """Close all open issues"""
+        self.close_issues()
+        return True
+
+
 class EdiIssue(models.AbstractModel):
     """EDI Issue-Tracked Object
 
